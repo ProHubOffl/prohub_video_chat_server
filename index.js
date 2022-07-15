1e15f55ec4f0115dfcf96b60d27de28896e95deb
Thiluxan <thiluxan@hotmail.co.uk>
2022-07-15 20:02:22 +0530

    Dependencies added and basic routing added

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const app = express();
+const io = require('socket.io')(http);
+const path = require('path');
+
+let socketList = {};
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+app.get('/ping', (req, res) => {
+  res
+    .send({
+      success: true,
+    })
+    .status(200);
+});
 
 const PORT = process.env.PORT || 3001;
 
